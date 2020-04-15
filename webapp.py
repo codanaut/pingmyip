08f993c49d16d5e177426cbee5c2d69ca633da26
codanaut <cody@codanaut.com>
2020-04-14 22:39:35 -0500

    should be fixed ip

--- webapp.py
+++ webapp.py
@@ -17,19 +17,34 @@ TEMPLATES_AUTO_RELOAD = True
 @app.route('/')
 def home():
 
-    ip = request.remote_addr
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
 
     return render_template('index.html', ip=ip)
 
 
 @app.route("/txt", methods=["GET"])
 def get_ip():
-    return request.remote_addr, 200
+
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
+
+    return ip, 200
     
 
 @app.route("/json", methods=["GET"])
 def json_ip():
-    return jsonify({'ip': request.remote_addr}), 200
+
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
+
+    return jsonify({'ip': ip}), 200
 
 @app.route('/test')
 def test():
